50f1c37dcfdb09df99d6731c01b4fb5a77f2c190
Worawut Boontan <worawut@theiconweb.com>
2020-04-30 20:45:17 +0700

    Rust Era

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,16 @@
-FROM rust:1.43
-WORKDIR /usr/src/app
-COPY . .
-RUN cargo install
-CMD ["remove_image_file"]
+FROM rust:1.43 AS builder
+WORKDIR /usr/src/
+RUN rustup target add x86_64-unknown-linux-musl
+
+RUN USER=root cargo new deciduously-com
+WORKDIR /usr/src/deciduously-com
+COPY Cargo.toml Cargo.lock ./
+RUN cargo build --release
+
+COPY src ./src
+RUN cargo install --target x86_64-unknown-linux-musl --path .
+
+FROM scratch
+COPY --from=builder /usr/local/cargo/bin/remove_image_file .
+USER 0
+CMD ["./remove_image_file"]
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,55 @@
+use chrono::DateTime;
+use chrono::Utc;
 use std::fs;
+use std::time::SystemTime;
+use time;
+static mut COUNTER: i32 = 0;
+
 fn main() {
-    let paths = fs::read_dir("./").unwrap();
+    let start = time::Instant::now();
+
+    match list_dir("./".to_string()) {
+        Err(e) => println!("Error {}", e),
+        Ok(_) => {}
+    }
+    unsafe {
+        println!("Total File : {:?}", COUNTER.to_string());
+    }
+    println!("{:?} seconds for whatever you did.", start.elapsed());
+}
+
+fn list_dir(param: String) -> std::io::Result<()> {
+    let paths = fs::read_dir(param).unwrap();
     for path in paths {
-        let path_result = path.unwrap();
-        if path_result.path().is_dir() {
-            println!("Directory {}", path_result.path().display())
+        let path_result = path.unwrap().path();
+        if path_result.is_dir() {
+            let dir_name = path_result.display();
+            match list_dir(dir_name.to_string()) {
+                Err(e) => print!("Break Error {}", e),
+                Ok(_) => {}
+            }
         } else {
-            println!("File {}", path_result.path().display());
+            let metadata = path_result.metadata();
+            if let Ok(v) = metadata {
+                match v.created() {
+                    Ok(time) => {
+                        println!(
+                            "{:?} {:?}",
+                            system_time_to_date_time(time),
+                            path_result.display()
+                        );
+                    }
+                    Err(e) => println!("{:?}", e),
+                }
+            } else {
+                println!("Not supported on this platform or filesystem");
+            }
         }
     }
+    Ok(())
+}
+
+fn system_time_to_date_time(t: SystemTime) -> String {
+    let datetime: DateTime<Utc> = t.into();
+    return datetime.format("%d/%m/%Y %T").to_string();
 }
