ea38ed5f9386cbc5c92fec77538fda884f936d81
Worawut Boontan <worawut@theiconweb.com>
2020-05-04 20:47:22 +0700

    last code

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ use std::time::SystemTime;
 use time;
 static mut COUNTER: i32 = 0;
 
+static mut filestruct: Vec<FileStruct> = Vec::new();
+
+struct FileStruct {
+    name: String,
+    date_time: SystemTime,
+}
+
 fn main() {
     let start = time::Instant::now();
 
@@ -14,6 +21,11 @@ fn main() {
     }
     unsafe {
         println!("Total File : {:?}", COUNTER.to_string());
+        // filestruct.push(FileStruct {
+        //     name: "a".to_string(),
+        //     date_time: DateTime.now,
+        // });
+        // filestruct.sort_by_key(|d| d.date_time); // error[E0507]: cannot move out of borrowed content
     }
     println!("{:?} seconds for whatever you did.", start.elapsed());
 }
@@ -22,29 +34,34 @@ fn list_dir(param: String) -> std::io::Result<()> {
     let paths = fs::read_dir(param).unwrap();
     for path in paths {
         let path_result = path.unwrap().path();
+        let metadata = path_result.metadata();
         if path_result.is_dir() {
             let dir_name = path_result.display();
             match list_dir(dir_name.to_string()) {
                 Err(e) => print!("Break Error {}", e),
-                Ok(_) => {}
-            }
-        } else {
-            let metadata = path_result.metadata();
-            if let Ok(v) = metadata {
-                match v.created() {
-                    Ok(time) => {
-                        println!(
-                            "{:?} {:?}",
-                            system_time_to_date_time(time),
-                            path_result.display()
-                        );
+                Ok(_) => {
+                    if let Ok(v) = metadata {
+                        println!("{:?}", path_result.display());
                     }
-                    Err(e) => println!("{:?}", e),
                 }
-            } else {
-                println!("Not supported on this platform or filesystem");
             }
         }
+        // } else {
+        //     if let Ok(v) = metadata {
+        //         match v.created() {
+        //             Ok(time) => {
+        //                 // println!(
+        //                 //     "{:?} {:?}",
+        //                 //     system_time_to_date_time(time),
+        //                 //     path_result.display()
+        //                 // );
+        //             }
+        //             Err(e) => println!("{:?}", e),
+        //         }
+        //     } else {
+        //         println!("Not supported on this platform or filesystem");
+        //     }
+        // }
     }
     Ok(())
 }
